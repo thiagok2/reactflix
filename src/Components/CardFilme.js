5ac9ad7854c8aa869d239c554d015c9c0665eda8
NicolasSM2019 <nicolassabinocorreia@gmail.com>
2025-09-17 16:17:37 -0300

    card-filme

--- src/Components/CardFilme.js
+++ src/Components/CardFilme.js
@@ -1,17 +1,26 @@
 import { Link } from "react-router-dom";
+
 import "./CardFilme.css";
 import filmesService from "../Services/FilmesService";
+import { IoMdStar } from "react-icons/io";
 
-function CardFilme ({filme}) {
-    const handleAddClicados = () => {
+function CardFilme({ filme }) {
+
+    if (!filme) {
+        return null;
+    }
+
+    const handleAddClicado = () => {
         filmesService.addFilmeClicado(filme);
     }
 
     return (
-        <Link to={`/filme/${filme.id}`} onClick={handleAddClicados}>
-            <img src={filme.fotoThumbnail} className="fotofilme" title={filme.descricao}></img>
+        <Link className="card" to={`/filme/${filme.id}`} onClick={handleAddClicado}
+            style={{ backgroundImage: `url(${filme.fotoThumbnail})` }}>
+            <div className="card-heard">{filme.faixa_etaria}</div>
+            <div className="titulo-card-heard">{filme.titulo} <div className="nota-card-heard"><IoMdStar />{filme.nota_avaliacao} </div></div>
         </Link>
-    )
+    );
 }
 
 export default CardFilme;
