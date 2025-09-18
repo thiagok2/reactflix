3d29384f8f8b6bef3139bbfd94c34f55be5ab3e7
gustavo <159432484+qrsluiza@users.noreply.github.com>
2025-09-18 15:41:43 -0300

    filmepage

--- src/Components/FilmeHeader.js
+++ src/Components/FilmeHeader.js
@@ -1,15 +1,28 @@
 import './FilmeHeader.css'
+import { IoAddCircleOutline } from "react-icons/io5";
+import { FaPlay } from "react-icons/fa";
 import { Link } from 'react-router-dom'
 
 function FilmeHeader({ filme }) {
     return (
         <div className='PaiHeader'>
             <div className='infoFilme'>
+
+                <div className='NomeFilme'>{filme.titulo}</div>
+
                 <div className='Tempo'>Duração: {filme.duracao}</div>
+
+            <div className='caracteristicas'>
 
                 <div className='classificacao'>
                     <span className="idade">{filme.faixa_etaria}</span>
                 </div>
+
+                <div className='lancamento'>{filme.ano_lancamento}</div>
+                <div className='temporadas'>{filme.temporadas}</div>
+              
+            </div>
+               
                
                 <div className='sobre'>Gênero: {filme.genero}</div>
 
@@ -17,6 +30,17 @@ function FilmeHeader({ filme }) {
                     <div className='texto'>
                         {filme.sinopse}
                     </div>
+
+                    <div className="atores-container">{filme.elenco}</div>
+                 
+                     <div className='Botoes-detalhes'>
+        
+                          <span className='estilo-botao'><FaPlay color ="000000" className='Icone'/> Assistir</span>
+
+                           <span className='estilo-lista'> <IoAddCircleOutline color ="000000" className='Lista'/>Minha Lista </span>
+                     </div>
+
+                
 
                     <div className='card-menu'>
                         <Link className='cor-visao'>
