6f84d48dd03a1b7e2bce71af4298d6dad0ef00b2
Gustaxbjj <153648794+TalysG26@users.noreply.github.com>
2025-09-18 16:49:36 -0300

    UPDATE

---
+++ src/pages/PerfilPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BarraConfig from '../Components/BarraConfig';
+import './PerfilPage.css';
+
+function PerfilPage() {
+  return (
+    <div className='perfil-page'>
+      <h1>Perfil do Usuário 👤</h1>
+      <BarraConfig />
+      <p>Se você está vendo isso, a página está funcionando!</p>
+    </div>
+  );
+}
+
+export default PerfilPage;
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import ProfilesPage from "./pages/ProfilesPage";
+import PerfilPage from './pages/PerfilPage';
 import ConfigPage from "./pages/ConfigPage";
 import FilmePage from "./pages/FilmePage"; 
-import CatalogoPage from "./pages/CatalogoPage"; // importando o componente
+import CatalogoPage from "./pages/CatalogoPage"; 
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 
@@ -10,11 +11,14 @@ function MainRoutes() {
     return (
         <Routes> 
             <Route path="/" element={<ProfilesPage />} />
+              <Route path="/perfil" element={<PerfilPage />} />
+            
             <Route path="/home" element={<HomePage />} />
             <Route path="/config" element={<ConfigPage />} />
             <Route path="/filme" element={<FilmePage />} />
             <Route path="/filme/:id" element={<FilmePage />} /> 
-            <Route path="/catalogo" element={<CatalogoPage />} /> {/* rota adicionada */}
+            
+            <Route path="/catalogo" element={<CatalogoPage />} />
             <Route path="/catalogo/:tipo" element={<CatalogoPage />} />
             
             <Route path="/login" element={<LoginPage />} />
