eb0a89acf75a4d185695243e98eb1caf55217bd8
Givanildo <68856133+givanildsss444@users.noreply.github.com>
2025-09-24 11:01:49 -0300

    falta colocque se não houver nenhum comentario

--- src/Components/ComentariosContainer.js
+++ src/Components/ComentariosContainer.js
@@ -1,41 +1,61 @@
 import ComentariosService from '../Services/ComentariosService';
 import './ComentariosContainer.css'
 import { FaStar } from "react-icons/fa";
+import { useState, useEffect } from 'react';
 
 function ComentariosContainer({ filme }) {
 
   const comentarios = ComentariosService.getByFilmeId(filme.id);
+  const [tamanho, setTamanho] = useState(0)
+  setTamanho(comentarios.length)
+
+
+  useEffect(() => {
+    setTamanho(comentarios.length);
+  }, [comentarios]);
+
 
   return (
-    <div className="comentario-container">
-      {
+    tamanho > 0 ?
 
-        comentarios.map((comentario, idx) => (
+      <div className='all-container'>
 
-          <div className='comentario' key={idx}>
-            <div className="comentario-header">
-              <strong>{comentario.autor}</strong>
+        
+      <div className='info-avaliacao'>
+        <strong>Avaliação Média: </strong>
+        <strong className='avaliação-texto'>
+          <span> <FaStar className='estrela' /> {filme.nota_avaliacao}/10</span>
+        </strong>
+
+      
+      </div>
+    
+      <div className="comentario-container">
+
+
+        {
+          comentarios.map((comentario, idx) => (
+
+            <div className='comentario' key={idx}>
+              <div className="comentario-header">
+                <img src={comentario.avatar_foto} className='foto-avatar-comentario' alt="image-perfil-comentario"/>
+                <strong>{comentario.autor}</strong>
+              </div>
+
+              <p className="comentario-texto">{comentario.texto}</p>
+
+              <div className='estrelas'>
+                <span> <FaStar className='estrela' /> {comentario.avaliacao}/10</span>
+                <span className="comentario-data">{comentario.data_comentario}</span>
+              </div>
+
             </div>
 
-            <p className="comentario-texto">{comentario.texto}</p>
-
-            <span className="comentario-data">{comentario.data_comentario}</span>
-
-            <div className='estrelas'>
-              {
-                Array.from({ length: comentario.avaliacao }).map((_, i) => (
-                  <FaStar key={i} className='estrela' />
-                ))
-              }
-
-              <span>{comentario.avaliacao}</span>
-            </div>
-
-          </div>
-
-        ))
-      }
+          ))
+        }
+      </div>
     </div>
+      : null
   );
 }
 
