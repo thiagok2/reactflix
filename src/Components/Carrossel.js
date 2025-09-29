be2f9ecb35ac0bcf9ffd53518f9fde80dca53c63
thiagok2 <thiagok2@gmail.com>
2025-09-29 12:22:09 -0300

    merge

--- src/Components/Carrossel.js
+++ src/Components/Carrossel.js
@@ -1,13 +1,8 @@
 import CardFilme from "./CardFilme";
 import "./Carrossel.css";
 
-<<<<<<< HEAD
-function Carrossel ({ listadeFilmes, descricao, expandido = false }) {
-    return (
-=======
-function Carrossel ({listadeFilmes, descricao, pExpandido = false}) {
+function Carrossel ({listadeFilmes, descricao, expandido = false}) {
     return(
->>>>>>> 3718fb15452502a85b39687c01d24da873e86ea0
         <div className="minhaLista">
 
           
@@ -16,11 +11,7 @@ function Carrossel ({listadeFilmes, descricao, pExpandido = false}) {
             <div className="carrosel">
                 {
                     listadeFilmes.map((filme, idx) => 
-<<<<<<< HEAD
                         <CardFilme key={idx} filme={filme} expandido={expandido} />
-=======
-                        <CardFilme key={idx} filme={filme} expandido={pExpandido} />
->>>>>>> 3718fb15452502a85b39687c01d24da873e86ea0
                     )
                 }
             </div>     
