61de29b26e10b04f775d4bce46182ba1f1d8fbcd
Givanildo Lourenço <68856133+givanildsss444@users.noreply.github.com>
2025-09-29 12:15:08 -0300

    Feat comentarios (#15)
    * falta colocque se não houver nenhum comentario
    
    * Comentários modificado
    
    * featt-cometnario

--- src/Components/ComentariosContainer.js
+++ src/Components/ComentariosContainer.js
@@ -1,38 +1,59 @@
 import ComentariosService from '../Services/ComentariosService';
 import './ComentariosContainer.css'
 import { FaStar } from "react-icons/fa";
-
+import { useState, useEffect } from 'react';
+//coment
 function ComentariosContainer({ filme }) {
 
   const comentarios = ComentariosService.getByFilmeId(filme.id);
+  const [tamanho, setTamanho] = useState(0)
+
+
+  useEffect(() => {
+    setTamanho(comentarios.length);
+  }, [comentarios]);
+
+  if(tamanho == 0) return null;
 
   return (
-    <div className="comentario-container">
-      {
+    <div className='all-container'>
 
-        comentarios.map((comentario, idx) => (
+      <div className='info-avaliacao'>
+        <strong>Avaliação Média: </strong>
+        <strong className='avaliação-texto'>
+          <span> <FaStar className='estrela' /> {filme.nota_avaliacao}/10</span>
+        </strong>
 
-          <div className='comentario' key={idx}>
-            <div className="comentario-header">
-              <strong>{comentario.autor}</strong>
+
+      </div>
+
+      <div className="comentario-container">
+          <strong className='avalicao'>Avaliações</strong>
+
+          <div className='lista-comentarios'>
+        {
+          comentarios.map((comentario, idx) => (
+            
+            <div className='comentario' key={idx}>
+              <div className="comentario-header">
+                <img src={comentario.avatar_foto} className='foto-avatar-comentario' alt="image-perfil-comentario" />
+                <strong>{comentario.autor}</strong>
+              </div>
+
+              <p className="comentario-texto">{comentario.texto}</p>
+
+
+              <div className='estrelas'>
+                <span> <FaStar className='estrela' /> {comentario.avaliacao}/10</span>
+                <span className="comentario-data">{comentario.data_comentario}</span>
+              </div>
+
             </div>
 
-            <p className="comentario-texto">{comentario.texto}</p>
-
-            <span className="comentario-data">{comentario.data_comentario}</span>
-
-            <div className='estrelas'>
-              
-                  <FaStar className='estrela' />
-                
-
-              <span>{comentario.avaliacao}/10</span>
-            </div>
-
-          </div>
-
-        ))
-      }
+          ))
+        }
+        </div>
+      </div>
     </div>
   );
 }
